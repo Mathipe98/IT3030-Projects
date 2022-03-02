eba96e1c32be7fdcbae575f8ae37c2e1c2fd9751
Mathipe98 <56786723+Mathipe98@users.noreply.github.com>
2022-03-02 23:38:48 +0100

    99% done

--- Project 2/variational_autoencoder.py
+++ Project 2/variational_autoencoder.py
@@ -51,7 +51,7 @@ class VAE:
                 training=False)
             # Create a callback for stopping if we don't get any progress
             callback = tf.keras.callbacks.EarlyStopping(
-                monitor='loss', min_delta=0.0001, patience=100, verbose=0, restore_best_weights=True
+                monitor='loss', min_delta=0.0001, patience=1000, verbose=0, restore_best_weights=True
             )
 
             for channel in range(x_train_all_channels.shape[-1]):
@@ -59,7 +59,7 @@ class VAE:
                 x_train = x_train_all_channels[:, :, :, [channel]]
                 x_test = x_test_all_channels[:, :, :, [channel]]
 
-                self.model.fit(x=x_train, y=x_train, batch_size=1024, epochs=epochs,
+                self.model.fit(x=x_train, y=x_train, batch_size=512, epochs=epochs,
                                validation_data=(x_test, x_test), callbacks=[callback])
             # Save weights and leave
             self.model.save_weights(filepath=self.file_name)
@@ -123,34 +123,57 @@ class VAE:
             ax.get_yaxis().set_visible(False)
         plt.show()
 
-    def anomaly_detection(self) -> None:
+    def anomaly_detection(self, filename: str) -> None:
         if not self.done_training:
             # Model is not trained yet...
             raise ValueError("Model is not trained; cannot detect anomalies")
-        bce = keras.losses.BinaryCrossentropy()
+        n_random_samples = 1000
+        bce = keras.losses.BinaryCrossentropy(reduction=tf.keras.losses.Reduction.NONE)
         testing_data, testing_labels = self.generator.get_full_data_set(
             training=False)
+        n_testing_examples = testing_data.shape[0]
         no_channels = testing_data.shape[-1]
-        example_loss_map = []
-        for i in range(len(testing_data)):
-            print(i)
-            example = testing_data[i]
-            label = testing_labels[i]
-            example = example[np.newaxis, :]
-            reconstruction = np.zeros(shape=(testing_data.shape[1:]))
-            for channel in range(no_channels):
-                a = self.model.predict(example[:,:,:,[channel]])
-                reconstruction[:,:,[channel]] += a[0,:,:,:]
-            loss = bce(example, reconstruction).numpy()
-            example_loss_map.append((reconstruction, label, loss))
-            if i == 3000:
-                break
-        sorted_map = sorted(example_loss_map, key=lambda tup: tup[2], reverse=True)
-        anomaly_examples = np.array([anomaly[0] for anomaly in sorted_map[:10]])
-        anomaly_labels = np.array([anomaly[1] for anomaly in sorted_map[:10]])
-        print(f"Highest losses:\n {[anomaly[2] for anomaly in sorted_map[:10]]}")
-        print(f"Corresponding labels:\n {[anomaly[1] for anomaly in sorted_map[:10]]}")
-        visualize_pictures(anomaly_examples, anomaly_labels, filename='./figures/STACK_anomalies_3000.png')
+        probability_map = np.ones(shape=(n_testing_examples, 2))
+        for channel in range(no_channels):
+            channel_labels = []
+            for label in testing_labels:
+                if len(str(label)) < channel + 1:
+                    channel_labels.append(0)
+                else:
+                    channel_labels.append(int(str(label)[-1 - channel]) * 10 ** channel)
+            channel_labels = np.array(channel_labels)
+            # We need to calculate the predicted outcome of the normal vector
+            channel_probabilities = []
+            for i in range(testing_data.shape[0]):
+                print(i)
+                normal_vector = K.random_normal(shape=(n_random_samples,) + self.model.z_layer_input_shape, mean=0., stddev=1.)
+                normal_vector_outputs = self.model.decoder.predict(normal_vector)
+                image = testing_data[i]
+                label = testing_labels[i]
+                # Shape is 28x28x1n_testing_examples
+                image_repeated = np.repeat(image.reshape(-1, *image.shape), n_random_samples, axis=0).astype(float)
+                logpx_z = -bce(normal_vector_outputs, image_repeated)
+                px_z = np.exp(logpx_z)
+                px = np.mean(px_z)
+                channel_probabilities.append(px)
+            channel_probabilities = np.array(channel_probabilities)
+            probability_map[:,0] *= channel_probabilities
+            probability_map[:,1] += channel_labels
+        sorted_array = probability_map[probability_map[:,0].argsort()]
+        anomaly_losses = sorted_array[:20,0]
+        anomaly_labels = sorted_array[:20,1].astype(int)
+        anomaly_examples = []
+        for label in anomaly_labels:
+            indeces = np.where(testing_labels == label)
+            example_index = indeces[0][0]
+            verify_label = testing_labels[example_index]
+            assert verify_label == label, "Index of example doesn't match"
+            anomaly_examples.append(testing_data[example_index])
+        anomaly_examples = np.array(anomaly_examples)
+        anomaly_recreations = self.predict(anomaly_examples)
+        print(f"Lowest probabilities:\n {anomaly_losses}")
+        print(f"Corresponding labels:\n {anomaly_labels}")
+        visualize_pictures(anomaly_examples, anomaly_labels, anomaly_recreations, filename=filename)
 
 def showcase_mono() -> None:
     print(f"\n======== SHOWCASING MONO-CHROMATIC IMAGES ========\n")
@@ -159,35 +182,50 @@ def showcase_mono() -> None:
     verifier = VerificationNet(force_learn=False)
     verifier.train(gen, epochs=100)
     vae = VAE(generator=gen, force_relearn=False)
-    vae.train(epochs=2000)
+    vae.train(epochs=1000)
     print(f"\nVisualizing training images...")
     vae.visualize_training_results()
-    print(f"\nVisualizing testing images...")
+    print(f"\nVisualizing testing images...\n")
     vae.visualize_testing_results()
-    vae.test_accuracy(verifier, tolerance=0.8)
+    print(f"\nCalculating accuracy..\n")
+    vae.test_accuracy(verifier=verifier, tolerance=0.8)
+    print(f"\nGenerating images..\n")
     vae.generate()
 
-def showcase_stack() -> None:
+def showcase_colour() -> None:
+    print(f"\n======== SHOWCASING COLOUR IMAGES ========\n")
     gen = StackedMNISTData(
         mode=DataMode.COLOR_BINARY_COMPLETE, default_batch_size=2048)
     verifier = VerificationNet(force_learn=False)
     verifier.train(gen, epochs=100)
     vae = VAE(generator=gen, force_relearn=False)
-    vae.train(epochs=2000)
+    vae.train(epochs=1000)
+    print(f"\nVisualizing training images...")
     vae.visualize_training_results()
+    print(f"\nVisualizing testing images...\n")
     vae.visualize_testing_results()
-    vae.test_accuracy(verifier, tolerance=0.5)
+    print(f"\nCalculating accuracy..\n")
+    vae.test_accuracy(verifier=verifier, tolerance=0.5)
+    print(f"\nGenerating images..\n")
     vae.generate()
 
 def showcase_anomalies() -> None:
-    gen = StackedMNISTData(
+    print(f"\n======== SHOWCASING ANOMALY DETECTION ========\n")
+    gen1 = StackedMNISTData(
         mode=DataMode.MONO_BINARY_MISSING, default_batch_size=2048)
-    verifier = VerificationNet(force_learn=False)
-    verifier.train(gen, epochs=100)
-    vae = VAE(generator=gen, force_relearn=True, file_name='./models/vae_anomaly_model')
-    vae.train(epochs=2000)
+    gen2 = StackedMNISTData(
+        mode=DataMode.COLOR_BINARY_MISSING, default_batch_size=2048)
+    vae1 = VAE(generator=gen1, force_relearn=False, file_name='./models/vae_anomaly_model')
+    vae1.train(epochs=10000)
+    vae2 = VAE(generator=gen2, force_relearn=False, file_name='./models/vae_anomaly_model')
+    vae2.train(epochs=10000)
+    print(f"\nDetecting mono-chromatic anomalies..\n")
+    vae1.anomaly_detection(filename='./figures/vae_mono_anomalies.png')
+    print(f"\nDetecting colour anomalies..\n")
+    #vae2.anomaly_detection(filename='./figures/vae_colour_anomalies.png')
 
 
 if __name__ == "__main__":
-    showcase_mono()
-    showcase_stack()
+    # showcase_mono()
+    # showcase_stack()
+    showcase_anomalies()
