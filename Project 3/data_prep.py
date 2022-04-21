c4c06038ac93745d10469e3911afa6ae52b186f3
Mathipe98 <56786723+Mathipe98@users.noreply.github.com>
2022-04-21 16:41:21 +0200

    Made working dataset-creator

--- Project 3/data_prep.py
+++ Project 3/data_prep.py
@@ -11,11 +11,12 @@ from typing import Tuple
 
 from model import get_lstm_model
 
+
 class Agent:
 
     def __init__(self, min_scale: int = 0, max_scale: int = 1, resolution: int = 15,
-                 n_prev: int = 96,
-                 batch_size: int = 64) -> None:
+                 n_prev: int = 96, start_index: int = 1, batch_size: int = 64,
+                 target: str = "y") -> None:
         if resolution != 5 and resolution != 15:
             raise ValueError(
                 f'Resolution must be either 5 or 15, got {resolution}')
@@ -33,10 +34,12 @@ class Agent:
         self.max_scale = max_scale
         self.resolution = resolution
         self.n_prev = n_prev
+        self.start_index = start_index
+        self.batch_size = batch_size
+        self.target = target
         # If resolution is 15 minutes, then we need 8*15 = 120 minutes = 2 hrs.
         # Else we need 24 predictions (24*5=120) to fill the 2 hours
         self.n_preds = 8 if resolution == 15 else 24
-        self.batch_size = batch_size
         self.x_scaler, self.y_scaler = self.setup_scalers()
 
     def setup_scalers(self) -> Tuple[MinMaxScaler, MinMaxScaler]:
@@ -48,10 +51,11 @@ class Agent:
         x_scaler = StandardScaler()
         y_scaler = StandardScaler()
         return x_scaler, y_scaler
-    
+
     def fit_scalers_to_df(self, df: pd.DataFrame) -> None:
         if "y" not in df.columns:
-            raise ValueError('Dataframe must have a column named "y" when fitting scalers')
+            raise ValueError(
+                'Dataframe must have a column named "y" when fitting scalers')
         x, y = df.drop("y", axis=1), df[["y"]]
         self.x_scaler.fit(x)
         self.y_scaler.fit(y)
@@ -63,7 +67,8 @@ class Agent:
         try:
             return self.x_scaler.transform(data)
         except:
-            raise RuntimeError('transform_x was called before scalers were fit to data')
+            raise RuntimeError(
+                'transform_x was called before scalers were fit to data')
 
     def transform_y(self, data: pd.DataFrame = None) -> np.ndarray:
         # Scale y
@@ -72,7 +77,8 @@ class Agent:
         try:
             return self.y_scaler.transform(data)
         except:
-            raise RuntimeError('transform_y was called before scalers were fit to data')
+            raise RuntimeError(
+                'transform_y was called before scalers were fit to data')
 
     def inverse_transform_x(self, data: np.ndarray) -> np.ndarray:
         return self.x_scaler.inverse_transform(data)
@@ -80,76 +86,98 @@ class Agent:
     def inverse_transform_y(self, data: np.ndarray) -> np.ndarray:
         return self.y_scaler.inverse_transform(data)
 
-    def process_df_to_xy(self, df: pd.DataFrame, target: str, training: bool = True) -> Tuple[np.ndarray, np.ndarray]:
-        if df is None:
-            raise ValueError('Dataframe not provided in process_df_to_xy')
-        if training:
-            if self.resolution == 15:
-                # Remove 2 rows for every 3rd row if we use every 15 minutes
-                df = df.copy().iloc[::3]
-            x, y = df.drop(target, axis=1), df[[target]]
-            # print(f"y in processing function: {y}")
-            # If processing training data, then transform (scale) and add previous_y
-            x = self.transform_x(x)
-            print(f"x after transformation: {x}")
-            y = self.transform_y(y).reshape(-1)
-            print(f"y after transformation: {y}")
-            # Manually shift the series forward by 1
-            previous_y = np.insert(y, 0, 0)
-            previous_y = np.delete(previous_y, -1)
-            x = np.insert(x, -1, previous_y, axis=1)
+    def make_training_dataset(self, data: pd.DataFrame) -> tf.data.Dataset:
+        print("Generating training dataset, therefore start_index has no effect.")
+        x = data.drop(columns=self.target)
+        y = data[self.target]
+        # Extract all rows apart from the very last one (because we don't have its "correct" value)
+        features = np.array(x, dtype=np.float32)[:-1]
+        labels = np.array(y, dtype=np.float32)
+        # Roll the labels array s.t. they (timewise) correspond to the correct inputs
+        labels = np.roll(labels, shift=-self.n_prev, axis=0)[:-1]
+        # Note: first n_prev elements of labels go unused
+        ds = tf.keras.utils.timeseries_dataset_from_array(
+            data=features,
+            targets=labels,
+            sequence_length=self.n_prev,
+            shuffle=False,
+            batch_size=self.batch_size)
+        return ds
+
+    def make_testing_dataset(self, data: pd.DataFrame) -> tf.data.Dataset:
+        idx1 = max(self.start_index - self.n_prev, 0)
+        idx2 = min(self.start_index, len(data))
+        # Print indeces
+        print(f"idx1: {idx1}, idx2: {idx2}")
+        x = data.drop(columns=self.target)[idx1:idx2]
+        print(f"Resulting x: {x}")
+        features = np.array(x, dtype=np.float32)
+        print(f"Actual target with these indices: {data[self.target][idx2]}")
+        if self.n_prev < self.start_index:
+            seq_len = self.n_prev
         else:
-            x = df
-            x = self.transform_x(x)
-            # y = dummy data for the generator (has no functional purpose)
-            y = np.zeros(len(x))
-        return x, y
+            seq_len = self.start_index
+        ds = tf.keras.utils.timeseries_dataset_from_array(
+            data=features,
+            targets=None,
+            sequence_length=seq_len,
+            shuffle=False,
+            batch_size=self.batch_size)
+        return ds
 
-    def get_generator(self, df: pd.DataFrame, target: str, training: bool = True) -> TimeseriesGenerator:
-        x, y = self.process_df_to_xy(df, target, training)
-        print(f"x generated during get_generator: {x}")
-        print(f"y generated during get_generator: {y}")
-        bsize = self.batch_size
-        print(f'Using batch size {bsize}\n')
-        return TimeseriesGenerator(x, y, length=self.n_prev, batch_size=bsize)
-
-    def train(self, df: pd.DataFrame, target: str="y", model: Sequential=None, epochs: int=10) -> History:
+    def train(self, df: pd.DataFrame, model: Sequential = None, epochs: int = 10) -> History:
         if model is None:
             model = get_lstm_model()
-        gen = self.get_generator(df, target, training=True)
         callbacks = EarlyStopping(
             monitor='loss',
             patience=3,
-            restore_best_weights=True
+            restore_best_weights=False
         )
-        return model.fit(gen, epochs=epochs, callbacks=[callbacks])
+        ds = self.make_training_dataset(df)
+        return model.fit(ds, epochs=epochs, callbacks=[callbacks])
 
-    def predict_2hrs(self, df: pd.DataFrame, model=None, start_index: int=1) -> np.ndarray:
-        if start_index < 1:
-            raise ValueError('start_index must be >= 1')
+    def predict_2hrs(self, df: pd.DataFrame, model=None) -> np.ndarray:
         if model is None:
             model = get_lstm_model()
         # Add a column to the test-dataframe with the predicted values
+        # They will be initialized randomly since we update them as we go
         df['previous_y'] = np.random.uniform(low=0, high=1, size=len(df))
         n_prev = self.n_prev
         # Set the previous y value at start index to the model prediction
         results = []
-        for i in range(start_index, min(len(df), start_index+self.n_preds)):
-            # Note: use max/min to calculate the correct indices at which to slice the dataframe as well as reshape
-            #i-n_prev+1
+        for i in range(self.start_index, min(len(df), self.start_index+self.n_preds)):
             idx1 = max(i - n_prev, 0)
             idx2 = min(i, len(df))
             df_slice = df[idx1: idx2].to_numpy()
             # Get size of df_slice
             df_slice = df_slice.reshape(1, min(n_prev, i), len(df.columns))
-            result = model(df_slice).numpy().astype(float)[0][0]
-            print(f"Result from prediction {i}: {result}\nResult datatype: {type(result)}\n")
-            df.loc[i+1, 'previous_y'] = result
+            # We will get a 3D output with batch size 1 -> resize to 2D
+            result = model(df_slice).numpy().reshape(df_slice.shape[1], -1)
+            # We now have shape [timesteps, predictions]. Set this as previous_y
+            df.loc[idx1+1: idx2, 'previous_y'] = result
             results.append(result)
         # Turn results into a dataframe with column "y"
         results = pd.DataFrame(data=results, columns=['y'])
         return self.transform_y(results)
-    
-    def visualize_results(self, y_true: pd.DataFrame, y_pred: np.ndarray, start_index: int=1) -> None:
-        results = pd.DataFrame({'y_true':y_true.values[start_index:start_index+self.n_preds],'y_pred':y_pred.ravel()})
-        results.plot(figsize=(20,8))
+
+    def visualize_results(self, y_true: pd.DataFrame, y_pred: np.ndarray, start_index: int = 1) -> None:
+        results = pd.DataFrame(
+            {'y_true': y_true.values[start_index:start_index+self.n_preds], 'y_pred': y_pred.ravel()})
+        results.plot(figsize=(20, 8))
+
+
+if __name__ == '__main__':
+    df_train = pd.read_csv(
+        './Project 3/no1_train.csv').drop("start_time", axis=1)
+    df_val = pd.read_csv(
+        './Project 3/no1_validation.csv').drop("start_time", axis=1)
+    agent = Agent(n_prev=10,
+              resolution=5,
+              start_index=10,
+              batch_size=64,
+              target='y'
+              )
+    print(df_val.head(20))
+    model = get_lstm_model()
+    agent.predict_2hrs(df_val, model)
+    # agent.train(df_train, model, epochs=5)
--- Project 3/google_window.py
+++ Project 3/google_window.py
@@ -11,41 +11,48 @@ from model import get_lstm_model
 class WindowGenerator():
     def __init__(self, n_prev: int, n_preds: int, resolution: int,
                  start_index: int = 1, batch_size: int = 32, target: str = "y"):
-        if n_preds > batch_size:
-            raise ValueError("n_preds must be <= batch_size; cannot sample from batch larger than number of samples.")
         self.n_prev = n_prev
         self.n_preds = n_preds
         self.resolution = resolution
         self.start_index = start_index
         self.batch_size = batch_size
         self.target = target
-
-    def make_dataset(self, data: pd.DataFrame, training: bool = True) -> tf.data.Dataset:
-        if training:
-            x = data.drop(columns=self.target)
-            y = data[self.target]
-            # Extract all rows apart from the very last one (because we don't have its "correct" value)
-            features = np.array(x, dtype=np.float32)[:-1]
-            labels = np.array(y, dtype=np.float32)
-            labels = np.roll(labels, shift=-idx2, axis=0)[:-1]
-        else:
-            idx1 = max(self.start_index - self.n_prev, 0)
-            idx2 = min(self.start_index, len(data))
-            # Print indeces
-            print(f"idx1: {idx1}, idx2: {idx2}")
-            x = data.drop(columns=self.target)[idx1:idx2]
-            print(f"Resulting x: {x}")
-            features = np.array(x, dtype=np.float32)
-            labels = None
-            print(f"Actual target with these indices: {data[self.target][idx2]}")
-        print(features.shape)
+    
+    def make_training_dataset(self, data: pd.DataFrame) -> tf.data.Dataset:
+        print("Generating training dataset, therefore start_index has no effect.")
+        x = data.drop(columns=self.target)
+        y = data[self.target]
+        # Extract all rows apart from the very last one (because we don't have its "correct" value)
+        features = np.array(x, dtype=np.float32)[:-1]
+        labels = np.array(y, dtype=np.float32)
+        labels = np.roll(labels, shift=-self.n_prev, axis=0)[:-1]
         # Roll the labels array s.t. they (timewise) match the inputs, i.e. that features
         # contain examples t_0 -> t_k, and y then contains t_k+1
         ds = tf.keras.utils.timeseries_dataset_from_array(
             data=features,
             targets=labels,
             sequence_length=self.n_prev,
-            sampling_rate=1,
+            shuffle=False,
+            batch_size=self.batch_size)
+        return ds
+
+    def make_testing_dataset(self, data: pd.DataFrame) -> tf.data.Dataset:
+        idx1 = max(self.start_index - self.n_prev, 0)
+        idx2 = min(self.start_index, len(data))
+        # Print indeces
+        print(f"idx1: {idx1}, idx2: {idx2}")
+        x = data.drop(columns=self.target)[idx1:idx2]
+        print(f"Resulting x: {x}")
+        features = np.array(x, dtype=np.float32)
+        print(f"Actual target with these indices: {data[self.target][idx2]}")
+        if self.n_prev < self.start_index:
+            seq_len = self.n_prev
+        else:
+            seq_len = self.start_index
+        ds = tf.keras.utils.timeseries_dataset_from_array(
+            data=features,
+            targets=None,
+            sequence_length=seq_len,
             shuffle=False,
             batch_size=self.batch_size)
         return ds
@@ -60,11 +67,18 @@ if __name__ == '__main__':
     df_val = pd.read_csv(
         './Project 3/no1_validation.csv').drop("start_time", axis=1)
     print(df_val.head(11))
-    w2 = WindowGenerator(n_prev=2, n_preds=1, resolution=5,
-                         start_index=10, batch_size=2, target='y')
+    w2 = WindowGenerator(n_prev=50, n_preds=50, resolution=5,
+                         start_index=50, batch_size=64, target='y')
     print(f"df_val index 10 answer: {df_val['y'][10]}")
-    tf_dataset = w2.make_dataset(df_val, training=False)
-    print(tf_dataset)
-
+    training_ds = w2.make_training_dataset(df_train[0:100])
+    testing_ds = w2.make_testing_dataset(df_val)
     model = get_lstm_model()
-    # model.evaluate(tf_dataset)
+    print(list(testing_ds))
+    # model.fit(training_ds, epochs=1)
+    for val_tensor in testing_ds:
+        print("H")
+        print(val_tensor.shape)
+        # print(b.shape)
+        result = model(val_tensor).numpy().shape
+        print(f"Result: {result}")
+        break
