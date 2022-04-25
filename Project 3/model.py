d65d5329410d8091917ba633ea061430178c9f6e
Mathipe98 <56786723+Mathipe98@users.noreply.github.com>
2022-04-25 20:23:24 +0200

    Best hyperparams so far

--- Project 3/model.py
+++ Project 3/model.py
@@ -8,13 +8,13 @@ from keras.layers import LSTM, Dense, Dropout, Flatten
 
 def get_lstm_model() -> Sequential:
     model = Sequential()
-    model.add(LSTM(units=256, return_sequences=True))
+    # model.add(LSTM(units=256, return_sequences=True))
     model.add(LSTM(units=128, return_sequences=True))
     model.add(LSTM(units=64, return_sequences=False))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(32, activation='relu'))
+    model.add(Dense(64))
+    model.add(Dense(32))
     model.add(Dense(1))
-    opt = tf.keras.optimizers.Adam(learning_rate=0.0001)
+    opt = tf.keras.optimizers.Adam(learning_rate=0.0005)
     model.compile(loss='mse', optimizer=opt, metrics=['mean_absolute_error'])
     return model
     
