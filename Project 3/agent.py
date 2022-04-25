4e8296d88bc553fadcc4bb63ec20c6c1888f4798
Mathipe98 <56786723+Mathipe98@users.noreply.github.com>
2022-04-25 15:35:49 +0200

    Manually reworked training dataset

--- Project 3/agent.py
+++ Project 3/agent.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -11,12 +12,15 @@ from typing import Tuple
 
 from model import get_lstm_model
 
+np.set_printoptions(edgeitems=50, linewidth=10000)
+
 
 class Agent:
 
     def __init__(self, min_scale: int = 0, max_scale: int = 1, resolution: int = 15,
                  n_prev: int = 96, start_index: int = 96, batch_size: int = 64,
-                 target: str = "y", verbose: bool=False, filepath: str='./models/LSTM_model') -> None:
+                 target: str = "y", verbose: bool = False,
+                 model: Sequential = None, filepath: str = './models/LSTM_model') -> None:
         if start_index < n_prev-1:
             raise ValueError(
                 f'Start index ({start_index}) must be greater than or equal to n_prev-1 ({n_prev-1})')
@@ -32,53 +36,41 @@ class Agent:
         self.batch_size = batch_size
         self.target = target
         self.verbose = verbose
+        self.model = model if model is not None else get_lstm_model()
         self.filepath = filepath
         # If resolution is 15 minutes, then we need 8*15 = 120 minutes = 2 hrs.
         # Else we need 24 predictions (24*5=120) to fill the 2 hours
         self.pred_timesteps = 8 if resolution == 15 else 24
-        self.x_scaler = MinMaxScaler(feature_range=(self.min_scale, self.max_scale))
-        self.y_scaler = MinMaxScaler(feature_range=(self.min_scale, self.max_scale))
-    
+        self.scaler = MinMaxScaler(feature_range=(self.min_scale, self.max_scale))
+
     def add_previous_y_to_df(self, df: pd.DataFrame) -> pd.DataFrame:
         # Add column to x, called previous_y, which is the last y value
         df = df.assign(previous_y=df[self.target].shift(1)).fillna(0)
         return df
-
-    def fit_scalers_to_df(self, df: pd.DataFrame) -> None:
-        if self.target not in df.columns:
-            raise ValueError(
-                f'Dataframe must have target column when fitting scalers (missing "{self.target}")')
-        x, y = df.drop(self.target, axis=1), df[[self.target]]
-        self.x_scaler.fit(x)
-        self.y_scaler.fit(y)
     
-    def transform_df(self, df: pd.DataFrame) -> pd.DataFrame:
-        x, y = df.drop(self.target, axis=1), df[[self.target]]
-        trans_x = self.x_scaler.transform(x)
-        trans_y = self.y_scaler.transform(y)
-        if self.verbose:
-            print(f"DF before transformation:\n{df.head(20)}")
-        result_df = pd.DataFrame(trans_x, columns=x.columns)
-        # Add y to the df
-        result_df[self.target] = trans_y
-        if self.verbose:
-            print(f"DF after transformation:\n{result_df.head(20)}")
-        return result_df
+    def fit_scaler(self, df: pd.DataFrame) -> None:
+        self.scaler.fit(df)
 
-    def inverse_transform_y(self, data: pd.DataFrame) -> np.ndarray:
-        return self.y_scaler.inverse_transform(data)
+    def transform_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        result_df = pd.DataFrame(self.scaler.transform(df), columns=df.columns)
+        return result_df
+    
+    def inverse_transform_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        return pd.DataFrame(self.scaler.inverse_transform(df), columns=df.columns)
 
     def make_training_dataset(self, df: pd.DataFrame) -> tf.data.Dataset:
         if self.verbose:
-            print(f'Making training dataset...')
+            print(f'\n===== CALLING MAKE_TRAINING_DATASET =====')
         x, y = df.drop(columns=self.target), df[[self.target]]
         if self.verbose:
-            print(f"y after transformation in making training dataset: {y}")
+            print(f"\n\nx in make_training_dataset:\n{x.head()}")
+            print(f"y in make_training_dataset:\n{y.head()}\n\n")
         # Extract all rows apart from the very last one (because we don't have its "correct" value)
         features = np.array(x, dtype=np.float32)[:-1]
         labels = np.array(y, dtype=np.float32)
         # Roll the labels array s.t. they (timewise) correspond to the correct inputs
         labels = np.roll(labels, shift=-self.n_prev+1, axis=0)[:-1]
+        print(f"Features shape:\n{features.shape}\nLabels shape:\n{labels.shape}")
         ds = tf.keras.utils.timeseries_dataset_from_array(
             data=features,
             targets=labels,
@@ -87,10 +79,26 @@ class Agent:
             shuffle=False,
             batch_size=self.batch_size)
         return ds
+    
+    def make_dataset(self, df: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
+        if self.verbose:
+            print(f'\n===== CALLING MAKE_DATASET =====')
+        x, y = df.drop(columns=self.target), df[[self.target]]
+        x_train = []
+        y_train = []
+        for i in range(0, len(df)-self.n_prev-1, 1):
+            if i % 10000 == 0 and self.verbose:
+                print(f"i: {i}")
+            inputs = x.loc[i+1:i+self.n_prev].to_numpy()
+            target = y.loc[i+self.n_prev].to_numpy()
+            x_train.append(inputs)
+            y_train.append(target)
+        x_train = np.array(x_train)
+        y_train = np.array(y_train)
+        print('Finished making dataset.\n')
+        return x_train, y_train
 
-    def train(self, df: pd.DataFrame, model: Sequential = None, epochs: int = 10) -> History:
-        if model is None:
-            model = get_lstm_model()
+    def train(self, df: pd.DataFrame, epochs: int = 10) -> History:
         cb1 = EarlyStopping(
             monitor='loss',
             patience=3,
@@ -103,24 +111,20 @@ class Agent:
         if self.resolution == 15:
             # Drop 2 out of every 3 rows
             df = df.iloc[::3]
-        ds = self.make_training_dataset(df)
-        return model.fit(ds, epochs=epochs, callbacks=[cb1, cb2])
-    
-    def predict(self, df: pd.DataFrame, index_to_predict: int, model: Sequential = None) -> float:
-        if model is None:
-            model = get_lstm_model()
+        x_train, y_train = self.make_dataset(df)
+        return self.model.fit(x_train, y_train, epochs=epochs, callbacks=[cb1, cb2])
+
+    def predict(self, df: pd.DataFrame, index_to_predict: int) -> float:
         row_start_index = max(0, index_to_predict-self.n_prev+1)
-        if self.verbose:
-            print(f"Predicting index {index_to_predict} based on previous DF segment:\n{df.loc[row_start_index:index_to_predict]}\n")
-        x = np.expand_dims(df.loc[row_start_index:index_to_predict].to_numpy(), axis=0)
-        model_pred = model(x).numpy()
+        x = np.expand_dims(
+            df.loc[row_start_index:index_to_predict].to_numpy(), axis=0)
+        model_pred = self.model(x).numpy()
         return float(model_pred[0][0])
 
-    def predict_n_timesteps(self, df: pd.DataFrame, model=None, n_timesteps: int=None) -> np.ndarray:
-        if model is None:
-            model = get_lstm_model()
-        if 'y' in df.columns:
-            df = df.drop('y', axis=1)
+    def predict_n_timesteps(self, df: pd.DataFrame, n_timesteps: int = None) -> pd.Series:
+        df = df.copy()
+        if self.target in df.columns:
+            df = df.drop(self.target, axis=1)
         if n_timesteps is None:
             n_timesteps = self.pred_timesteps
         if self.resolution == 15:
@@ -130,44 +134,61 @@ class Agent:
             # Right now, this produces an error.
         results = []
         if self.verbose:
-            print('\n===== STARTING PREDICTION =====\n')
+            print('\n===== CALLING PREDICT_N_TIMESTEPS =====')
         for i in range(self.start_index, self.start_index+n_timesteps):
-            result = self.predict(df, i, model)
-            if self.verbose:
-                print(f"Prediction for index {i}:\n{result}")
+            result = self.predict(df, i)
             df.loc[i+1, 'previous_y'] = result
             results.append(result)
-        # Turn results into a dataframe with column "y"
-        results = pd.DataFrame(data=results, columns=['y'])
-        # Return the inverse transformed version
-        return self.inverse_transform_y(results)
+        return self.inverse_transform_df(df)[self.start_index+1:self.start_index+n_timesteps+1]['previous_y']
 
-    def visualize_results(self, y_true: pd.DataFrame, y_pred: np.ndarray, n_timesteps: int=None) -> None:
+    def visualize_results(self, y_true: pd.Series, y_pred: pd.Series, n_timesteps: int = None) -> None:
         if n_timesteps is None:
             n_timesteps = self.pred_timesteps
-        y_true = self.inverse_transform_y(y_true)[self.start_index:self.start_index+n_timesteps]
+        # [self.start_index:self.start_index+n_timesteps]
+        y_true = y_true.to_numpy()[self.start_index:self.start_index+n_timesteps]
+        print(len(y_true))
+        y_pred = y_pred.to_numpy()
         results = pd.DataFrame(
             {'y_true': y_true.ravel(), 'y_pred': y_pred.ravel()})
         results.plot(figsize=(20, 8))
-
+        plt.show()
+        
 
 if __name__ == '__main__':
     df_train = pd.read_csv(
         './Project 3/no1_train.csv').drop("start_time", axis=1)
     df_val = pd.read_csv(
         './Project 3/no1_validation.csv').drop("start_time", axis=1)
-    agent = Agent(n_prev=10,
-              resolution=5,
-              start_index=9,
-              batch_size=256,
-              target='y',
-              verbose=True
-              )
-    agent.fit_scalers_to_df(df_train)
+    # Move column 'y' in df_train and df_val to the end
+    df_train = df_train.reindex(
+        columns=df_train.drop('y', axis=1).columns.tolist() + ['y'])
+    df_val = df_val.reindex(
+        columns=df_val.drop('y', axis=1).columns.tolist() + ['y'])
+    print(df_train.head())
+    print(df_val.head())
+    agent = Agent(
+        min_scale=0,
+        max_scale=1,
+        n_prev=24,
+        resolution=5,
+        start_index=24,
+        batch_size=64,
+        target='y',
+        verbose=True
+    )
+    y_true = df_val['y']
+    agent.fit_scaler(df_train)
+    print(f"\nOriginal df_train:\n{df_train.head(15)}")
+    print(f"\nOriginal df_val:\n{df_val.head(15)}\n")
     df_train = agent.transform_df(df_train)
     df_val = agent.transform_df(df_val)
+    print(f"\nTransformed df_train:\n{df_train.head(15)}")
+    print(f"\nTransformed df_val:\n{df_val.head(15)}\n")
     df_train = agent.add_previous_y_to_df(df_train)
     df_val = agent.add_previous_y_to_df(df_val)
-    print(f"DF Val head:\n{df_val.head(agent.n_prev)}")
+    print(f"\nTrans df_train after prev_y:\n{df_train.head(15)}")
+    print(f"Trans df_val after prev_y:\n{df_val.head(15)}\n")
     agent.train(df_train, epochs=1)
-    agent.predict_n_timesteps(df_val, n_timesteps=4)
+    result = agent.predict_n_timesteps(df_val, n_timesteps=5)
+    print(f"Resulting pd Series:\n{result}\n")
+    agent.visualize_results(y_true=y_true, y_pred=result, n_timesteps=5)
--- Project 3/model.py
+++ Project 3/model.py
@@ -10,7 +10,7 @@ def get_lstm_model() -> Sequential:
     model = Sequential()
     model.add(LSTM(units=128, return_sequences=True, dropout=0.1))
     model.add(LSTM(units=64, return_sequences=False, dropout=0.1))
-    model.add(Dense(64))
+    # model.add(Dense(64))
     model.add(Dense(1))
     model.compile(loss='mse', optimizer='adam', metrics=['mean_absolute_error'])
     return model
