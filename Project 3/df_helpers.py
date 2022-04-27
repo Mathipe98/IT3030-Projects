91c3ccf118ad8e4d33c03b7cdb5cf0f77a84223a
Mathipe98 <56786723+Mathipe98@users.noreply.github.com>
2022-04-27 17:58:19 +0200

    Finished project!

--- Project 3/df_helpers.py
+++ Project 3/df_helpers.py
@@ -5,18 +5,6 @@ import pandas as pd
 
 from scipy.interpolate import splev, splrep
 
-
-def get_outliers(df: pd.DataFrame, col: str, threshold: float = 3) -> pd.DataFrame:
-    """
-    Returns a dataframe of rows that are outliers in the given column.
-    """
-    q1 = df[col].quantile(0.25)
-    q3 = df[col].quantile(0.75)
-    iqr = q3 - q1
-    lower_bound = q1 - (threshold * iqr)
-    upper_bound = q3 + (threshold * iqr)
-    return df[(df[col] < lower_bound) | (df[col] > upper_bound)]
-
 def get_rows_between(df: pd.DataFrame, feature: str, lower: float, upper: float) -> pd.DataFrame:
     """
     Returns a dataframe of rows that are between the given lower and upper bounds.
--- Project 3/model.py
+++ Project 3/model.py
@@ -6,21 +6,60 @@ import tensorflow as tf
 from keras import Sequential
 from keras.layers import LSTM, Dense
 
-# Note to self:
-#   Best parameters for y-pred:
-#    LSTM_units = [128, 64]
-#    Dense_units = [64, 32]
-# NO DROPOUT
+
+"""
+NOTES REGARDING PARAMETERS:
+
+
+    - Following LSTM units have been tested:
+        - [512, 256, 128, 64, 32, 16]
+
+        - The thought of the low-value units was that the model only seems to care much about the
+            closest values in time, therefore the activation passed between the LSTM cells (i.e. the 'units' value)
+            could be low. However after much testing, the optimal units values were 128 and 64.
+
+    
+    - Following Dense units have been tested:
+        - [256, 128, 64, 32, 16, 8]
+
+        - They were tried with different activation functions, including relu, sigmoid, and linear.
+        - It seemed that the activation functions in these layers did not improve model performance,
+            therefore the linear activation function was used.
+    
+
+    - The final LSTM layer was initially used with
+
+                [return_sequences=True],
+    
+        i.e. output for every input. However this complicated the learning process and resulted in a
+        worse model. Therefore it was dropped to just return the final output.
+    
+
+    - The learning rate was tested on the following values:
+        - [0.0001, 0.001, 0.01, 0.1]
+
+        - A lower learning-rate was tried with a more substantial model, i.e. one with more LSTM and Dense layers.
+        - However the addition of layers did not improve the model, therefore the learning rate was kept at 0.001 for the final model
+    
+
+    - Finally, an
+
+        L2 REGULARIZATION
+
+    was added to the model. This was done to prevent overfitting.
+        - However, it seems that the model actually NEEDS overfitting in order to properly mimic the oscillation found
+            during training. Therefore the model tended to 'average' its losses with regularization, resulting in more
+            'smooth' curves. Reg was therefore dropped in order for the model to better mimic the oscillation.
+"""
 
 def get_model(lstm_units: List[int], dense_units: List[int], lr: float) -> Sequential:
     model = Sequential()
-    regularizer = tf.keras.regularizers.l1_l2(l1=0.01, l2=0.01)
     for i in lstm_units:
         if i == lstm_units[-1]:
             seq = False
         else:
             seq = True
-        model.add(LSTM(units=i, return_sequences=seq, dropout=0.1, kernel_regularizer=regularizer))
+        model.add(LSTM(units=i, return_sequences=seq, dropout=0.1))
     for i in dense_units:
         model.add(Dense(i))
     model.add(Dense(1))
